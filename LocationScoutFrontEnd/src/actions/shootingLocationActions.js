c5e3561f935d2b8b011eb96ed61578a209a55626
mankenavenkatesh <vmankena@BANL15013fa7c.local>
2018-10-05 08:34:32 +0530

    Authentication Frontend changes

--- LocationScoutFrontEnd/src/actions/shootingLocationActions.js
+++ LocationScoutFrontEnd/src/actions/shootingLocationActions.js
@@ -1,67 +1,77 @@
-import locationsApi from '../api/LocationsApi';
-import * as types from '../types';
-import {browserHistory} from 'react-router'; 
+import locationsApi from "../api/LocationsApi";
+import * as types from "../types";
+import { browserHistory } from "react-router";
 
-
-  export function fetchMyLocations(locationOwnerId) {   
-      // debugger 
-    return function(dispatch) {
-      return locationsApi.getLocationsById(locationOwnerId).then(myLocations => {        
+export function fetchMyLocations(locationOwnerId) {
+  // debugger
+  return function(dispatch) {
+    return locationsApi
+      .getLocationsById(locationOwnerId)
+      .then(myLocations => {
         dispatch(setMyLocations(myLocations));
-      return myLocations;
-      }).catch(error => {
-        console.log(error);
-      });
-    };
-  }
-
-  export function setMyLocations(myLocations) {       
-    return {type: types.SET_MY_LOCATIONS, myLocations};
-  }
-
-  export function  fetchLocation(locationId) {
-    return function(dispatch){
-      return locationsApi.getLocationById(locationId).then(location => {
-        dispatch(locationFetched(location));
-      }).catch(error => {
-        console.log(error);
+        return myLocations;
       })
-    };
-  }
+      .catch(error => {
+        console.log(error);
+      });
+  };
+}
 
-  export function locationFetched(location){
-    return {type : types.LOCATION_FETCHED, location};
-    }
+export function setMyLocations(myLocations) {
+  return { type: types.SET_MY_LOCATIONS, myLocations };
+}
 
-  export function saveLocation(locationOwnerId, location) {    
-    return function(dispatch) {
-      return locationsApi.addUserLocation(locationOwnerId, location).then(newlocation => {        
+export function fetchLocation(locationId) {
+  return function(dispatch) {
+    return locationsApi
+      .getLocationById(locationId)
+      .then(location => {
+        dispatch(locationFetched(location));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+}
+
+export function locationFetched(location) {
+  return { type: types.LOCATION_FETCHED, location };
+}
+
+export function saveLocation(location) {
+  return function(dispatch) {
+    return locationsApi
+      .addUserLocation(location)
+      .then(newlocation => {
         dispatch(addMyLocation(newlocation));
-      return newlocation;
-      }).catch(error => {
+        return newlocation;
+      })
+      .catch(error => {
         console.log(error);
       });
-    };
-  }
+  };
+}
 
-  export function addMyLocation(newlocation) {              
-    return {type: types.ADD_MY_LOCATION, newlocation};
-  }
+export function addMyLocation(newlocation) {
+  return { type: types.ADD_MY_LOCATION, newlocation };
+}
 
-
-  export function deleteLocation(locationOwnerId, locationId) {    
+export function deleteLocation(locationOwnerId, locationId) {
+  // debugger
+  return function(dispatch) {
     // debugger
-    return function(dispatch) {
-      // debugger
-      return locationsApi.deleteLocation(locationOwnerId, locationId).then(locationDeleted => {        
+    return locationsApi
+      .deleteLocation(locationOwnerId, locationId)
+      .then(locationDeleted => {
         dispatch(locationDeleted(locationId));
-      return locationDeleted;
-      }).catch(error => {
+        return locationDeleted;
+      })
+      .catch(error => {
         console.log(error);
       });
-    };
-  }
+  };
+}
 
-  export function locationDeleted(locationId){
-    return {type : types.LOCATION_DELETED, locationId};
-  }
+export function locationDeleted(locationId) {
+  return { type: types.LOCATION_DELETED, locationId };
+}
--- LocationScoutFrontEnd/src/containers/Dashboard/index.js
+++ LocationScoutFrontEnd/src/containers/Dashboard/index.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom'
-import LoginHeader from '../LoginHeader';
-import DashboardContent from '../DashboardContent';
-import DashboardNavigation from '../DashboardNavigation';
-import AddLocation from '../AddLocation';
-import EditLocation from '../EditLocation';
-import MyLocations from '../MyLocations';
-import MyProfile from '../MyProfile';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import LoginHeader from "../LoginHeader";
+import DashboardContent from "../DashboardContent";
+import DashboardNavigation from "../DashboardNavigation";
+import AddLocation from "../AddLocation";
+import EditLocation from "../EditLocation";
+import MyLocations from "../MyLocations";
+import MyProfile from "../MyProfile";
 class Dashboard extends Component {
-  render() {    
+  render() {
     return (
-        <div>                       
-            <div id="dashboard" >
-              <DashboardNavigation />            
-                <Switch>                                        
-                  <Route exact path='/dashboard/' component = {DashboardContent} />
-                  <Route exact path='/dashboard/addLocation' component={AddLocation}/>               
-                  <Route exact path='/dashboard/editLocation/:locationId' component={EditLocation}/> 
-                  <Route exact path='/dashboard/myLocations' component={MyLocations}/>  
-                  <Route exact path='/dashboard/myProfile' component={MyProfile}/>               
-                </Switch>                      
+      <div>
+        <div id="dashboarddiv">
+          <DashboardNavigation />
+          <Switch>
+            <Route exact path="/dashboard/" component={DashboardContent} />
+            <Route
+              exact
+              path="/dashboard/addLocation"
+              component={AddLocation}
+            />
+            <Route
+              exact
+              path="/dashboard/editLocation/:locationId"
+              component={EditLocation}
+            />
+            <Route
+              exact
+              path="/dashboard/myLocations"
+              component={MyLocations}
+            />
+            <Route exact path="/dashboard/myProfile" component={MyProfile} />
+          </Switch>
         </div>
-        </div>
+      </div>
     );
   }
 }
