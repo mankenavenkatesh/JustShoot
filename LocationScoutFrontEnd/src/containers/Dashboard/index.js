236802b98c23a97a37f749d15627d7766ffddc70
mankenavenkatesh <mankenavenkatesh@gmail.com>
2019-01-20 17:21:52 +0530

    Dashboard Navigation Responsiveness

--- LocationScoutFrontEnd/src/containers/Dashboard/index.js
+++ LocationScoutFrontEnd/src/containers/Dashboard/index.js
@@ -16,29 +16,22 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div>
-        <div id="dashboarddiv">
-          <DashboardNavigation />
-          <Switch>
-            <Route exact path="/dashboard/" component={DashboardContent} />
-            <Route
-              exact
-              path="/dashboard/addLocation"
-              component={AddLocation}
-            />
-            <Route
-              exact
-              path="/dashboard/editLocation/:locationId"
-              component={EditLocation}
-            />
-            <Route
-              exact
-              path="/dashboard/myLocations"
-              component={MyLocations}
-            />
-            <Route exact path="/dashboard/myProfile" component={MyProfile} />
-          </Switch>
-        </div>
+      <div id="dashboard">
+        <a href="/dashboard" class="dashboard-responsive-nav-trigger active">
+          <i class="fa fa-reorder" /> Dashboard Navigation
+        </a>
+        <DashboardNavigation />
+        <Switch>
+          <Route exact path="/dashboard/" component={DashboardContent} />
+          <Route exact path="/dashboard/addLocation" component={AddLocation} />
+          <Route
+            exact
+            path="/dashboard/editLocation/:locationId"
+            component={EditLocation}
+          />
+          <Route exact path="/dashboard/myLocations" component={MyLocations} />
+          <Route exact path="/dashboard/myProfile" component={MyProfile} />
+        </Switch>
       </div>
     );
   }
--- LocationScoutFrontEnd/src/containers/Footer/index.js
+++ LocationScoutFrontEnd/src/containers/Footer/index.js
@@ -3,119 +3,112 @@ import Backtotop from "../Backtotop/";
 class Footer extends React.Component {
   render() {
     return (
-      <div>
-        <div id="footer" class="sticky-footer">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-5 col-sm-6">
-                <img class="footer-logo" src="images/logo.png" alt="" />
-                <br />
-                <p>
-                  Morbi convallis bibendum urna ut viverra. Maecenas quis
-                  consequat libero, a feugiat eros. Nunc ut lacinia tortor morbi
-                  ultricies laoreet ullamcorper phasellus semper.
-                </p>
-              </div>
-
-              <div class="col-md-4 col-sm-6 ">
-                <h4>Helpful Links</h4>
-                <ul class="footer-links">
-                  <li>
-                    <a href="#">Login</a>
-                  </li>
-                  <li>
-                    <a href="#">Sign Up</a>
-                  </li>
-                  <li>
-                    <a href="#">My Account</a>
-                  </li>
-                  <li>
-                    <a href="#">Add Listing</a>
-                  </li>
-                  <li>
-                    <a href="#">Pricing</a>
-                  </li>
-                  <li>
-                    <a href="#">Privacy Policy</a>
-                  </li>
-                </ul>
-
-                <ul class="footer-links">
-                  <li>
-                    <a href="#">FAQ</a>
-                  </li>
-                  <li>
-                    <a href="#">Blog</a>
-                  </li>
-                  <li>
-                    <a href="#">Our Partners</a>
-                  </li>
-                  <li>
-                    <a href="#">How It Works</a>
-                  </li>
-                  <li>
-                    <a href="#">Contact</a>
-                  </li>
-                </ul>
-                <div class="clearfix" />
-              </div>
-
-              <div class="col-md-3  col-sm-12">
-                <h4>Contact Us</h4>
-                <div class="text-widget">
-                  <span>12345 Little Lonsdale St, Melbourne</span> <br />
-                  Phone: <span>(123) 123-456 </span>
-                  <br />
-                  E-Mail:
-                  <span>
-                    {" "}
-                    <a href="#">
-                      <span
-                        class="__cf_email__"
-                        data-cfemail="365950505f555376534e575b465a531855595b"
-                      >
-                        [email&#160;protected]
-                      </span>
-                    </a>{" "}
-                  </span>
-                  <br />
-                </div>
-
-                <ul class="social-icons margin-top-20">
-                  <li>
-                    <a class="facebook" href="#">
-                      <i class="icon-facebook" />
-                    </a>
-                  </li>
-                  <li>
-                    <a class="twitter" href="#">
-                      <i class="icon-twitter" />
-                    </a>
-                  </li>
-                  <li>
-                    <a class="gplus" href="#">
-                      <i class="icon-gplus" />
-                    </a>
-                  </li>
-                  <li>
-                    <a class="vimeo" href="#">
-                      <i class="icon-vimeo" />
-                    </a>
-                  </li>
-                </ul>
-              </div>
+      <div id="footer" className="dark">
+        {/* Main */}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-5 col-sm-6">
+              <h4>Just Shoot</h4>
+              <p>
+                Justshoot is a marketplace for Shooting locations, Costumes
+                ,Equipment,Crew etc to make shooting easy. we believe creators
+                should only focus on creativity. Rest all should be taken care.
+              </p>
             </div>
-
-            <div class="row">
-              <div class="col-md-12">
-                <div class="copyrights">
-                  © 2019 Justshoot. All Rights Reserved.
-                </div>
+            <div className="col-md-4 col-sm-6 ">
+              <h4>Helpful Links</h4>
+              <ul className="footer-links">
+                <li>
+                  <a href="#">Login</a>
+                </li>
+                <li>
+                  <a href="#">Sign Up</a>
+                </li>
+                <li>
+                  <a href="#">My Account</a>
+                </li>
+                <li>
+                  <a href="#">Add Listing</a>
+                </li>
+                <li>
+                  <a href="#">Pricing</a>
+                </li>
+                <li>
+                  <a href="#">Privacy Policy</a>
+                </li>
+              </ul>
+              <ul className="footer-links">
+                <li>
+                  <a href="#">FAQ</a>
+                </li>
+                <li>
+                  <a href="#">Blog</a>
+                </li>
+                <li>
+                  <a href="#">Our Partners</a>
+                </li>
+                <li>
+                  <a href="#">How It Works</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+              <div className="clearfix" />
+            </div>
+            <div className="col-md-3  col-sm-12">
+              <h4>Contact Us</h4>
+              <div className="text-widget">
+                <span>Kondapur, Hyderabad</span> <br />
+                Phone: <span>8880746896 </span>
+                <br />
+                E-Mail:
+                <span>
+                  {" "}
+                  <a href="#">
+                    <span
+                      className="__cf_email__"
+                      data-cfemail="137c75757a707653766b727e637f763d707c7e"
+                    >
+                      [email&nbsp;protected]
+                    </span>
+                  </a>{" "}
+                </span>
+                <br />
+              </div>
+              <ul className="social-icons margin-top-20">
+                <li>
+                  <a className="facebook" href="#">
+                    <i className="icon-facebook" />
+                  </a>
+                </li>
+                <li>
+                  <a className="twitter" href="#">
+                    <i className="icon-twitter" />
+                  </a>
+                </li>
+                <li>
+                  <a className="gplus" href="#">
+                    <i className="icon-gplus" />
+                  </a>
+                </li>
+                <li>
+                  <a className="vimeo" href="#">
+                    <i className="icon-vimeo" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* Copyright */}
+          <div className="row">
+            <div className="col-md-12">
+              <div className="copyrights">
+                © 2017 Listeo. All Rights Reserved.
               </div>
             </div>
           </div>
         </div>
-        <Backtotop />
       </div>
     );
   }
